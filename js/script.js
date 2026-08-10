ef9245a3913375d2e2abc6f6260b94d1f5a20d40
martinSadir21 <martin.sadir21@gmail.com>
2026-08-10 16:41:13 -0300

    feat(js): agregar clase card a la tarjeta con setAttribute
    Closes #3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //1- Agregar a la tarjeta, el atributo "class" con el valor "card"
-
+document.getElementById("tarjeta").setAttribute("class", "card");
 
 //2- Agregar a la imagen, el atributo "src" con el valor "https://www.youtube.com/img/desktop/yt_1200.png"
 
